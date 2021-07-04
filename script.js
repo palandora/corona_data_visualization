ef2d98512998806a9968654465f9650e6d6b41f0
palandora <bernd@dotsandlines.io>
2021-07-04 15:51:05 +0200

    added toggle js

---
+++ script.js
@@ -0,0 +1,28 @@
+let TOGGLE_CLICKED = false;
+
+function toggle(){
+    const toggle = document.querySelector('.toggle');
+    toggle.addEventListener('click',()=>{
+        if(!TOGGLE_CLICKED){
+            toggle.classList.add('active');
+            TOGGLE_CLICKED = true;
+            switchTheme(TOGGLE_CLICKED);
+        }else{
+            toggle.classList.remove('active');
+            TOGGLE_CLICKED = false;
+            switchTheme(TOGGLE_CLICKED);
+        }
+    });
+}
+
+function switchTheme(toggle_clicked){
+    if(toggle_clicked){
+        document.body.classList.remove('light');
+        document.body.classList.add('dark');
+    }else{
+        document.body.classList.remove('dark');
+        document.body.classList.add('light');
+    }
+}
+
+toggle();
